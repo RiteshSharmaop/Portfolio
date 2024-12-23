36b57bb7c9108f3bbf00f9831105318846a08557
Ritesh Sharma <riteshrsharma71@gmail.com>
2024-12-23 20:51:05 +0530

    Done

--- Frontend/Frontend/src/Components/Projects.jsx
+++ Frontend/Frontend/src/Components/Projects.jsx
@@ -28,14 +28,15 @@ export default function Projects() {
           <ProjectBox
             title="MapSee"
             navigateTo="/wip"
-            desc="A Backend Web App Where user can register and login to do tweet, upload video and can do like, subscribe share, and track history. Developed a scalable RESTful API (CRUD) using Node.js, Express, and MongoDB. Implemented secure JWT authentication, data validation middleware, and conducted thorough endpoint testing to ensure reliability, high performance, and modularity. Handling videos vie a Cloudinary and Multer "
+            desc="A robust web application developed using the MERN stack, Leaflet.js, and Socket.io, enabling students to log in with their email, password, and college credentials. The platform offers real-time GPS-based bus tracking, allowing students to view live bus locations, calculate optimal navigation routes from their current location to the bus and college, and ensure efficient travel management. Integrated with secure user authentication, session handling, and WebSocket-based real-time updates, the application enhances usability and scalability while providing a seamless logout feature"
             imgLink={img3}
             />
 
           <ProjectBox
             title="RideGo"
             navigateTo="/wip"
-            desc="A Backend Web App Where user can register and login to do tweet, upload video and can do like, subscribe share, and track history. Developed a scalable RESTful API (CRUD) using Node.js, Express, and MongoDB. Implemented secure JWT authentication, data validation middleware, and conducted thorough endpoint testing to ensure reliability, high performance, and modularity. Handling videos vie a Cloudinary and Multer "
+            desc=" 
+A comprehensive ride-hailing application built with the MERN stack (MongoDB, Express.js, React.js, Node.js) that replicates the functionality of popular platforms like Uber. The app enables users to book rides by entering pickup and drop-off locations with real-time route visualization using Map APIs. Key features include user authentication, fare estimation, real-time driver tracking, and ride status updates. Drivers can manage ride requests, navigate optimized routes, and track their trips. The app leverages Socket.io for real-time communication, ensuring smooth coordination between riders and drivers. This project showcases advanced web and mobile development techniques, integrating secure APIs, scalable architecture, and interactive user interfaces for a seamless ride-booking experience."
             imgLink={rideit}
             />
           <ProjectBox
